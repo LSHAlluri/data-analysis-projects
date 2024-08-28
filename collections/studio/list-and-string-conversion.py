29a06993cead26f2dae15d10542d18a38b969234
LSHAlluri <lakshmi_alluri@outlook.com>
2024-08-28 00:21:31 -0500

    collections studio

--- collections/studio/list-and-string-conversion.py
+++ collections/studio/list-and-string-conversion.py
@@ -4,20 +4,49 @@ proto_list3 = "space delimited string"
 proto_list4 = "Comma-spaces, might, require, typing, caution"
 
 strings = [proto_list1, proto_list2, proto_list3, proto_list4]
-
+print("\n")
 # a) Use the 'in' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
+for string in strings:
+    
+# b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array 
+# into a new comma separated string.
+    if ',' in string:
+        print(string, " uses commas.")
 
+        # split it into an array
+        stringTOlist = string.split(',')
+        print(stringTOlist, " converted to list.")
 
-# b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
+        # reverse the entries
+        for index in range(len(stringTOlist)):
+            stringTOlist[index] = stringTOlist[index][::-1]
+# e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as 
+# part “b”, making sure that the extra spaces are NOT part of the final string.
+            stringTOlist[index] = stringTOlist[index].strip()
+        print(stringTOlist, "Reversed string list")
 
+        # join the array into a new comma separated string.
+        reversed_combined_string = ','.join(stringTOlist)
+        print(reversed_combined_string, " Reversed string list to comma separated string.\n ")
 
+# c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, 
+# and then join the array into a new comma separated string.
+    elif ';' in string:
+        print(string, " uses semicolons.")
+        semicolon_List = string.split(';')
+        print(semicolon_List, " converted to list.")
+        semicolon_List.sort()
+        print(semicolon_List, " sorted in ascending.")
+        reversed_semicolon_str = ','.join(semicolon_List)
+        print(reversed_semicolon_str, " Reversed semicolon list into string.\n")
 
-# c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
-
-
-
-# d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
-
-
-
-# e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
+# d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the 
+# entries, and then join the array into a new space separated string.
+    elif ' ' in string:
+        print(string, " uses spaces.")
+        spaces_list = string.split()
+        print(spaces_list, " converted to list.")
+        spaces_list.sort(reverse=True)
+        print(spaces_list, "sorted in reverse order.")
+        reversed_spaces_Str = ' '.join(spaces_list)
+        print(reversed_spaces_Str, " Reversed spaces list into string.\n")
--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -1,20 +1,27 @@
 my_string = "LaunchCode"
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
-modified_string = my_string + my_string[0:3]
-modified_string = modified_string.replace(my_string[0:3],"",1)
+modified_string = my_string[3:] + my_string[0:3]
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
-print("The string {0} is modified to {1}.".format(my_string,modified_string))
+print("\nThe string {0} is modified to {1}.\n".format(my_string,modified_string))
 
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 n = int(input("Enter the number of letters to be relocated: "))
-usr_modified_string = my_string + my_string[0:n]
-usr_modified_string = usr_modified_string.replace(my_string[0:n],"",1)
-
-print("The string {0} is modified to {1}.".format(my_string,usr_modified_string))
+usr_modified_string = my_string[n:] + my_string[0:n]
+print("The string {0} is modified to {1}.\n".format(my_string,usr_modified_string))
 
 # c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 
 # 3 characters. Also, the template literal should note the error.
+n = int(input("Enter the number of letters to be relocated: "))
+if n > len(my_string):
+    err_msg = "The number is longer than the string!"
+    validated_modified_string = my_string[3:] + my_string[0:3]
+    print("{2}\nThe string {0} is modified to {1}.".format(my_string,validated_modified_string,err_msg))
+else:
+    validated_modified_string = my_string[n:] + my_string[0:n]
+    print("The string {0} is modified to {1}.".format(my_string,validated_modified_string))
+
+
 
